c057278a7e0f1240704cd8ce6662b2c508b9ffed
root <root@localhost.localdomain>
2019-09-11 18:01:48 +0300

    Initial commit

---
+++ src/com/string/Main.java
@@ -0,0 +1,10 @@
+package com.string;
+import java.util.Scanner;
+
+ public class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        System.out.println(s);
+    }
+}
